1b2435d9e08c3373e3cfce7d41df7a05369a47a8
itsevgenii <smazanovich@icloud.com>
2022-12-06 10:54:15 +0000

    added local env file and created PORT variable in app.js to make server listen

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { application } from "express";
 import logger from "morgan";
 import cors from "cors";
 
 import shoppingListRouter from "./routes/shoppingList.js";
-
+const PORT = 3005;
 const app = express();
 
 app.use(cors());
@@ -12,5 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/items", shoppingListRouter);
+
+
+app.listen(PORT, ()=>{console.log(`This server is running on ${PORT}`)})
 
 export default app;
